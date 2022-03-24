28e06a1b97fd0013b38971161042545110ee4d4b
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-03-24 10:56:57 +0900

    invalidateOptionMenu

--- ViewPager2/ViewPager2_BottomNavigation/README.md
+++ ViewPager2/ViewPager2_BottomNavigation/README.md
@@ -4,6 +4,14 @@
 
 ---
 1. <a href = "#content1">ViewPager2_BottomNavigation 연결 절차</a></br>
+-BottomNavigationView 에 들어갈 메뉴 생성</br>
+-xml 에 ViewPager2 태그와 BottomNavigationView 태그 생성</br>
+-프래그먼트 어댑터를 이용한 ViewPager2 세팅</br>
+-BottomNavigationView, ViewPager2 연결</br>
+
+2. <a href = "#content2">Fragment 에 따른 툴바 메뉴 변경</a></br>
+-invalidateOptionsMenu()</br>
+
 * <a href = "#ref">참고링크</a>
 ---
 ><a id = "content1">**1. ViewPager2_BottomNavigation 연결 절차**</a></br>
@@ -12,14 +20,9 @@
 **(1) BottomNavigationView 에 들어갈 메뉴 생성**</br>
 -res-menu-menu_bottom.xml
 <br></br>
-<br></br>
 **(2) xml 에 ViewPager2 태그와 BottomNavigationView 태그 생성**</br>
 
 ```xml
-    <androidx.appcompat.widget.AppCompatImageView
-        //...
-        />
-
     <com.google.android.material.bottomnavigation.BottomNavigationView
         android:id="@+id/bottomNavigation"
         android:layout_width="0dp"
@@ -30,8 +33,6 @@
         app:layout_constraintStart_toStartOf="parent" />
 ```
 <br></br>
-<br></br>
-
 **(3) 프래그먼트 어댑터를 이용한 ViewPager2 세팅**</br>
 -`class FragmentAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity)`
 
@@ -48,21 +49,29 @@ binding.viewPager.adapter = adapter
 
 ```
 <br></br>
-<br></br>
 **(4) BottomNavigationView, ViewPager2 연결**</br>
 
 ```kotlin
+val toolBarTitleList = listOf(
+    getString(R.string.fragment_a),
+    getString(R.string.fragment_b),
+    getString(R.string.fragment_c)
+)
+//BottomNavigation -> ViewPager2
 binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
     when(menuItem.itemId) {
         R.id.menuA -> {
+            binding.titleTextView.text = toolBarTitleList[0]
             binding.viewPager.currentItem = 0
             true
         }
         R.id.menuB -> {
+            binding.titleTextView.text = toolBarTitleList[1]
             binding.viewPager.currentItem = 1
             true
         }
         R.id.menuC -> {
+            binding.titleTextView.text = toolBarTitleList[2]
             binding.viewPager.currentItem = 2
             true
         }
@@ -71,15 +80,51 @@ binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
         }
     }
 }
+//ViewPager2 -> BottomNavigationView
 binding.viewPager.registerOnPageChangeCallback(
     object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
+            invalidateOptionsMenu()
+            binding.titleTextView.text = toolBarTitleList[position]
             binding.bottomNavigation.menu.getItem(position).isChecked = true
         }
     }
 )
 ```
+<br></br>
+<br></br>
+
+><a id = "content2">**2. Fragment 에 따른 툴바 메뉴 변경**</a></br>
+
+-**invalidateOptionsMenu()**</br>
+
+```kotlin
+
+//뷰페이저에 변화가 있을 때 CallBack 함수가 실행되는데 내부 invalidateOptionsMenu() 가 menu 를 파괴시킴
+binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+    override fun onPageSelected(position: Int) {
+        super.onPageSelected(position)
+        invalidateOptionsMenu()
+        binding.titleTextView.text = toolBarTitleList[position]
+        binding.bottomNavigation.menu.getItem(position).isChecked = true
+    }
+})
+
+//menu 가 파괴되면 onCreateOptionsMenu() 가 실행됨
+override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    val inflater: MenuInflater = menuInflater
+    inflater.inflate(R.menu.menu_toolbar, menu)
+    menu?.apply {
+        findItem(R.id.toolBarMenu1).isVisible = (binding.viewPager.currentItem == 0)
+        findItem(R.id.toolBarMenu2).isVisible = (binding.viewPager.currentItem == 1)
+        findItem(R.id.toolBarMenu3).isVisible = (binding.viewPager.currentItem == 2)
+    }
+    return true
+}
+
+```
+
 
 <br></br>
 <br></br>
--- ViewPager2/ViewPager2_BottomNavigation/app/src/main/java/com/example/viewpager2_bottomnavigation/activitiy/MainActivity.kt
+++ ViewPager2/ViewPager2_BottomNavigation/app/src/main/java/com/example/viewpager2_bottomnavigation/activitiy/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.viewpager2_bottomnavigation.activitiy
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.viewpager2_bottomnavigation.R
@@ -13,14 +17,36 @@ import com.example.viewpager2_bottomnavigation.fragments.CFragment
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         initFragment()
         initLinkBottomNaviWithViewPager2()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_toolbar, menu)
+        menu?.apply {
+            findItem(R.id.toolBarMenu1).isVisible = (binding.viewPager.currentItem == 0)
+            findItem(R.id.toolBarMenu2).isVisible = (binding.viewPager.currentItem == 1)
+            findItem(R.id.toolBarMenu3).isVisible = (binding.viewPager.currentItem == 2)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.toolBarMenu1 -> Toast.makeText(this, "ToolBar Menu 1", Toast.LENGTH_SHORT).show()
+            R.id.toolBarMenu2 -> Toast.makeText(this, "ToolBar Menu 2", Toast.LENGTH_SHORT).show()
+            R.id.toolBarMenu3 -> Toast.makeText(this, "ToolBar Menu 3", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     private fun initFragment() {
         val fragmentList = listOf(
@@ -34,18 +60,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initLinkBottomNaviWithViewPager2() {
+        val toolBarTitleList = listOf(
+            getString(R.string.fragment_a),
+            getString(R.string.fragment_b),
+            getString(R.string.fragment_c)
+        )
         binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
             when(menuItem.itemId) {
                 R.id.menuA -> {
                     binding.viewPager.currentItem = 0
+                    binding.titleTextView.text = toolBarTitleList[0]
                     true
                 }
                 R.id.menuB -> {
                     binding.viewPager.currentItem = 1
+                    binding.titleTextView.text = toolBarTitleList[1]
                     true
                 }
                 R.id.menuC -> {
                     binding.viewPager.currentItem = 2
+                    binding.titleTextView.text = toolBarTitleList[2]
                     true
                 }
                 else -> {
@@ -53,13 +87,13 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        binding.viewPager.registerOnPageChangeCallback(
-            object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                    binding.bottomNavigation.menu.getItem(position).isChecked = true
-                }
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                invalidateOptionsMenu()
+                binding.titleTextView.text = toolBarTitleList[position]
+                binding.bottomNavigation.menu.getItem(position).isChecked = true
             }
-        )
+        })
     }
 }
